9a180c7b7115ede00f8f7032657a188e5eb02fe4
coleknie <59704601+coleknie@users.noreply.github.com>
2022-05-30 00:58:39 -0400

    Add files via upload

---
+++ DFA.rkt
@@ -0,0 +1,145 @@
+#lang racket
+;lab04
+
+;DFA
+(define (DFA input Sigma S s0 delta F)
+  (define (invalidState? state S)
+    (cond ((null? S) #t)
+          (else
+           (cond ((equal? state (car S)) #f)
+                 (else (invalidState? state (cdr S)))))))
+
+  (define (nextState current transition transitions S)
+    (cond ((or (null? transitions) (invalidState? current S)) null)
+          (else
+           (cond ((and (equal? current (caar transitions)) (equal? transition (cadar transitions)))
+                  (caddar transitions))
+                 (else (nextState current transition (cdr transitions) S))))))
+
+  (define (iterate inputList current transitions S)
+    (cond ((null? (cdr inputList)) (nextState current (string (car inputList)) transitions S))
+          (else (iterate (cdr inputList) (nextState current (string (car inputList)) transitions S) transitions S))))
+
+  (define (finalState? state F)
+    (cond ((null? F) #f)
+          (else
+           (cond ((equal? state (car F)) #t)
+                 (else (finalState? state (cdr F)))))))
+
+  (define (letterInAlphabet? Sigma letter)
+    (cond ((null? Sigma) #f)
+          (else
+           (cond ((equal? (car Sigma) letter) #t)
+                 (else (letterInAlphabet? (cdr Sigma) letter))))))
+
+  (define (inputInAlphabet? inputList Sigma)
+    (cond ((null? inputList) #t)
+          (else
+           (cond ((letterInAlphabet? Sigma (string (car inputList))) (inputInAlphabet? (cdr inputList) Sigma))
+                 (else #f)))))
+
+  (cond ((equal? input "") (finalState? s0 F))
+  (else (define inputList (string->list input))
+        (define result (iterate inputList s0 delta S))
+  (and (not (null? result)) (finalState? result F) (inputInAlphabet? inputList Sigma)))))
+
+
+;NFA
+(define (NFA input Sigma Q q0 Delta F)
+  (define (nextState current transition transitions results)
+  (cond ((null? transitions) results)
+        (else
+               (cond ((and (equal? current (caar transitions)) (equal? transition (cadar transitions)))
+                      (nextState current transition (cdr transitions) (append-element results (caddar transitions)))
+                     )
+                     (else (nextState current transition (cdr transitions) results))))))
+(define (validState? state Q)
+  (cond ((null? Q) #f)
+        (else
+         (cond ((equal? state (car Q)) #t)
+               (else (validState? state (cdr Q)))))))
+(define (append-element ls y)
+  (cond((null? ls) (cons y null))
+    (else
+     (cond
+       ((null? (cdr ls)) (cons (car ls) (cons y null)))
+       (else (cons (car ls) (append-element (cdr ls) y)))))))
+
+(define (multipleNextStates currentStates transition transitions newStates)
+  (cond ((null? currentStates) '())
+        (else
+         (cond ((null? (cdr currentStates)) (append newStates (nextState (car currentStates) transition transitions '())))
+               (else (multipleNextStates (cdr currentStates) transition transitions (append newStates (nextState (car currentStates) transition transitions '()))))))))
+
+(define (iterate currentStates directions transitions Q)
+  (cond ((null? currentStates) '())
+        (else
+         (cond ((null? (cdr directions)) (multipleNextStates (removeBadStates currentStates Q '()) (string (car directions)) transitions '()))
+               (else (iterate (removeBadStates (multipleNextStates currentStates (string (car directions)) transitions '()) Q '()) (cdr directions) transitions Q))))))
+
+
+
+
+(define (hasFinalState? finishedStates F)
+  (cond ((null? (cdr finishedStates)) (finalState? (car finishedStates) F))
+        (else
+         (cond ((finalState? (car finishedStates) F) #t)
+               (else (hasFinalState? (cdr finishedStates) F))))))
+
+(define (finalState? state F)
+    (cond ((null? F) #f)
+          (else
+           (cond ((equal? state (car F)) #t)
+                 (else (finalState? state (cdr F)))))))
+
+  (define (letterInAlphabet? Sigma letter)
+    (cond ((null? Sigma) #f)
+          (else
+           (cond ((equal? (car Sigma) letter) #t)
+                 (else (letterInAlphabet? (cdr Sigma) letter))))))
+
+  (define (inputInAlphabet? inputList Sigma)
+    (cond ((null? inputList) #t)
+          (else
+           (cond ((letterInAlphabet? Sigma (string (car inputList))) (inputInAlphabet? (cdr inputList) Sigma))
+                 (else #f)))))
+
+(define (removeBadStates currentStates Q fixedStates)
+  (cond ((null? currentStates) '())
+        (else
+         (cond ((null? (cdr currentStates))
+                (cond ((validState? (car currentStates) Q) (append-element fixedStates (car currentStates)))
+                      (else fixedStates)))
+               (else
+                (cond ((validState? (car currentStates) Q) (removeBadStates (cdr currentStates) Q (append-element fixedStates (car currentStates))))
+                      (else (removeBadStates (cdr currentStates) Q fixedStates))))))))
+  (cond ((equal? input "") finalState? q0)
+  (else (define inputList (string->list input))
+        (define endStates (iterate (removeBadStates '(q0) Q '()) inputList Delta Q) )
+  (and (not (null? endStates)) (hasFinalState? endStates F) (inputInAlphabet? inputList Sigma)))))
+
+
+;TEST CASES
+(display "\nTEST CASES\n\n")
+
+
+;Test Cases: DFA
+(display "DFA\n")
+(DFA "0000" '("1" "0") '(q0 q1) 'q0 '((q0 "0" q0) (q0 "1" q1) (q1 "0" q1) (q1 "1" q1)) '(q1)) ;#f
+(DFA "0001" '("1" "0") '(q0 q1) 'q0 '((q0 "0" q0) (q0 "1" q1) (q1 "0" q1) (q1 "1" q1)) '(q1)) ;#t
+(DFA "" '("1" "0") '(q0 q1) 'q0 '((q0 "0" q0) (q0 "1" q1) (q1 "0" q1) (q1 "1" q1)) '(q1)) ;#f
+(DFA "2" '("1" "0") '(q0 q1) 'q0 '((q0 "0" q0) (q0 "1" q1) (q1 "0" q1) (q1 "1" q1)) '(q1)) ;#f
+(DFA "10111001101" '("1" "0") '(q0 q1) 'q0 '((q0 "0" q0) (q0 "1" q1) (q1 "0" q1) (q1 "1" q1)) '(q1)) ;#t
+
+
+;Test Cases: NFA
+(display "\nNFA\n")
+(NFA "aaab" '("a" "b") '(t1 t2 t3 t4 NA) 't1 
+    '(
+         (t1 "b" (t1)) (t1 "a" (t1 t2)) (t2 "b" (t3)) (t2 "a" (NA)) (t3 "b" (NA))
+         (t3 "a" (t4)) (t4 "a" (t4)) (t4 "b" (t4)) (NA "a" (NA)) (NA "b" (NA))
+     ) 
+    '(t4)) ;#f
+
+
+
---
+++ PDA.rkt
@@ -0,0 +1,154 @@
+#lang racket
+;Lab06
+
+(define-struct State (stack cur_state input))
+(define (finalstate? s0 F)
+  (cond
+    ((null? F) #f)
+    ((and (equal? (State-cur_state s0) (car F)) (zero? (string-length (State-input s0)))) #t)
+    (else (finalstate? s0 (cdr F)))))
+
+(define (PDAfinalstate? q0 F)
+  (cond
+    ((null? q0) #f)
+    ((finalstate? (car q0) F) #t)
+    (else (PDAfinalstate? (cdr q0) F))
+    )
+  )
+(define (push stack element)
+  (cons element stack))
+  
+(define (pop stack)
+  (cond ((null? stack)
+         null)
+        (else
+         (cdr stack))))
+
+(define (peek stack)
+  (cond ((null? stack)
+         null)
+        (else (car stack))))
+(define (nextStates stack input end next)
+  (cond
+    ((null? end) next)
+    (else (nextStates (make-State stack (car end) input) input (cdr end) (append (cons (make-State stack (car end) input) '()) next)))))
+
+(define (findTransition delta s0 trans_states)
+  
+  (cond
+    ((null? delta) trans_states)
+    
+    (else
+     
+    (define stateStack (State-stack s0))
+    (define stateState (State-cur_state s0))
+    (define stateInp (State-input s0))
+    (define trans (car delta))
+    (define start_state (car trans))
+    (define trans_input (cadr trans))
+    (define stackPop (caddr trans))
+    (define stackPush (cadddr trans))
+    (define next_state (car (cdr (cdr (cdr (cdr trans))))))
+     (cond
+        ((equal? stateState start_state)
+         (cond
+        ((and (equal? trans_input "") (equal? stackPop "") (equal? stackPush ""))
+        
+         (define newState (nextStates stateStack stateInp next_state null))
+         (cond ((null? trans_states) (findTransition (cdr delta) s0 newState))(else (findTransition (cdr delta) s0 (append trans_states newState)))))
+
+        ((and (not (equal? trans_input ""))  (not (equal? stateInp "")) (equal? stackPop "") (equal? stackPush "") (equal? trans_input (substring (State-input s0) 0 1)))
+         
+         (define newState (nextStates stateStack (substring stateInp 1) next_state null))
+                (cond ((null? trans_states) (findTransition (cdr delta) s0 newState))(else (findTransition (cdr delta) s0 (append trans_states newState))))
+         )
+
+        ((and (equal? trans_input "") (equal? stackPop "") (not (equal? stackPush "")))
+         
+         (define newState (nextStates (push stateStack stackPush) stateInp next_state null))
+         (cond ((null? trans_states) (findTransition (cdr delta) s0 newState))(else (findTransition (cdr delta) s0 (append trans_states newState)))))
+
+        ((and (equal? trans_input "") (not (equal? stackPop "")) (equal? stackPush "") (equal? (peek (State-stack s0)) stackPop))
+ 
+         (define newState (nextStates (pop stateStack) stateInp next_state null))
+                (cond ((null? trans_states) (findTransition (cdr delta) s0 newState))(else (findTransition (cdr delta) s0 (append trans_states newState)))))
+
+        ((and (not (equal? trans_input "")) (not (equal? stateInp "")) (equal? stackPop "") (not (equal? stackPush "")) (equal? trans_input (substring (State-input s0) 0 1)))
+                
+                (define newState (nextStates (push stateStack stackPush) (substring stateInp 1) next_state null))
+                (cond ((null? trans_states) (findTransition (cdr delta) s0 newState))(else (findTransition (cdr delta) s0 (append trans_states newState)))))
+
+        ((and (not (equal? trans_input "")) (not (equal? stateInp ""))(not (equal? stackPop "")) (equal? stackPush "")(equal? trans_input (substring (State-input s0) 0 1)) (equal? (peek (State-stack s0)) stackPop))
+         
+         (define newState (nextStates (pop stateStack) (substring stateInp 1) next_state null))
+                (cond ((null? trans_states) (findTransition (cdr delta) s0 newState))(else (findTransition (cdr delta) s0 (append trans_states newState))))
+         )
+
+        ((and (equal? trans_input "") (not (equal? stackPop "")) (not (equal? stackPush ""))(equal? (peek (State-stack s0)) stackPop))
+        
+         (define newState (nextStates (push (pop stateStack) stackPush) stateInp next_state null))
+                (cond ((null? trans_states) (findTransition (cdr delta) s0 newState))(else (findTransition (cdr delta) s0 (append trans_states newState)))))
+
+        ((and (not (equal? trans_input "")) (not (equal? stateInp "")) (not (equal? stackPop "")) (not (equal? stackPush ""))(equal? trans_input (substring (State-input s0) 0 1)) (equal? (peek (State-stack s0)) stackPop))
+         
+         (define newState (nextStates (push (pop stateStack) stackPush) (substring stateInp 1) next_state null))
+                (cond ((null? trans_states) (findTransition (cdr delta) s0 newState))(else (findTransition (cdr delta) s0 (append trans_states newState))))
+         )
+        (else
+         
+         (findTransition (cdr delta) s0 trans_states))))
+     (else
+      
+      (findTransition (cdr delta) s0 trans_states))))))
+
+
+(define (PDAfindTransition delta s0)
+  (cond
+    ((null? s0) null)
+    (else
+     (append (findTransition delta (car s0) '()) (PDAfindTransition delta (cdr s0))))
+    ))
+
+(define (sameStates? s0 s1)
+  (cond
+    ((null? s1) #t)
+    (else
+     (and (in? (car s1) s0) (sameStates? s0 (cdr s1))))
+    )
+  )
+(define (in? s s0)
+  (cond
+    ((null? s0) #f)
+    (else (or (state-equal? (car s0) s) (in? s (cdr s0))))
+    )
+  )
+(define (state-equal? s0 s1)
+  (and (equal? (State-stack s1) (State-stack s0)) (equal? (State-input s1) (State-input s0)) (equal? (State-cur_state s0) (State-cur_state s1)))
+ )
+(define (allInputempty? s0)
+  (cond
+    ((null? s0) #f)
+    (else (and (equal? "" (State-input (car s0) )) (allInputempty? (cdr s0))))))
+                       
+(define (PDA input Sigma Gamma S s0 Delta F)
+  (define (PDAHelper s0 Delta F)
+    (cond
+      ((PDAfinalstate? s0 F) #t)
+      (else
+       (define current_states (PDAfindTransition Delta s0))
+       (cond
+         ((sameStates? s0 current_states) #f)
+         ((allInputempty? current_states) #f)
+         (else
+          (PDAHelper current_states Delta F))))
+      )
+    )
+       
+       
+  (define initial_state (make-State '() s0 input))
+  (PDAHelper (cons initial_state null) Delta F))
+
+
+
+
+
